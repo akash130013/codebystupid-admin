71f132db8d08cd1420468223b8cea2c1658b79e1
akash130013 <akash130013@gmail.com>
2022-12-26 08:07:43 +0530

    fix: migration issue

---
+++ database/migrations/2022_12_26_023419_add_status_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('status')->default(1)->comment('1=>active, 0- inactive 3- deleted');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('status');
+        });
+    }
+};
